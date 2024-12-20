1e5ca0b9b9168830d629efe0588899c812ad4d31
Ethan Lu <luethan2025@outlook.com>
2024-12-20 13:13:39 -0800

    added a flag to clear pip cache on macOS

--- setup.sh
+++ setup.sh
@@ -1,5 +1,35 @@
 #!/bin/sh
 
+clear_cache=false # delete cache
+
+for arg in "$@"; do
+  case $arg in
+    --clear_cache)
+      clear_cache=true
+      shift
+      ;;
+    --help)
+      echo "Usage: $0 [--clear_cache]"
+      exit 0
+      ;;
+    *)
+      echo "Unknown option: $arg"
+      echo "Use --help for usage."
+      exit 1
+      ;;
+  esac
+done
+
+if [ "$clear_cache" = true ]; then
+  if [[ "$(uname -s)" == "Darwin" ]]; then
+    echo "rm -rf ~/Library/Caches/pip"
+    rm -rf ~/Library/Caches/pip
+  else
+    echo "Could not remove cache for non-macOS device"
+    echo "Skipping..."
+  fi
+fi
+
 python3 -m pip install --upgrade pip
 pip3 freeze | grep -v -E 'setuptools|pip' | xargs pip3 uninstall -y
 python3 -m ensurepip
