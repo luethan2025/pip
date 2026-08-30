6bd8477e6053311e12d1f1d29532955648c09270
Ethan Lu <luethan2025@outlook.com>
2026-08-29 17:30:39 -0700

    switched over to a basic cuda setup

--- setup.sh
+++ setup.sh
@@ -1,49 +1,9 @@
 #!/bin/sh
 
-clear_cache=false # delete cache
-
-for arg in "$@"; do
-  case $arg in
-    --clear_cache)
-      clear_cache=true
-      shift
-      ;;
-    --help)
-      echo "Usage: $0 [--clear_cache]"
-      exit 0
-      ;;
-    *)
-      echo "Unknown option: $arg"
-      echo "Use --help for usage."
-      exit 1
-      ;;
-  esac
-done
-
-if [ "$clear_cache" = true ]; then
-  if [[ "$(uname -s)" == "Darwin" ]]; then
-    echo "rm -rf ~/Library/Caches/pip"
-    rm -rf ~/Library/Caches/pip
-
-    echo "rm -rf ~/nltk_data"
-    rm -rf ~/nltk_data
-  else
-    echo "Could not remove cache for non-macOS device"
-    echo "Skipping..."
-  fi
-fi
+conda init
+conda activate base
 
 python3 -m pip install --upgrade pip
-pip3 freeze | grep -v -E 'setuptools|pip' | xargs pip3 uninstall -y
-python3 -m ensurepip
 
-pip3 install numpy
-pip3 install torch torchvision
-pip3 install transformers
-pip3 install scikit-learn
-pip3 install scipy
-pip3 install matplotlib
-pip3 install opencv-python
-pip3 install ipykernel
-pip3 install nltk && \
-  python3 -c "import nltk; nltk.download('punkt'); nltk.download('wordnet');"
+pip3 install datasets
+pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu128
